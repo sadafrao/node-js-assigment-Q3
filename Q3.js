b02fb96e52041f099af3a5f58ea7dd59c117e6e1
sadafrao <159582784+sadafrao@users.noreply.github.com>
2024-02-14 13:06:32 -0800

    Q3

---
+++ Q3.js
@@ -0,0 +1,6 @@
+"use strict";
+// 3. Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+let firstname = (" Miss sadaf");
+console.log(firstname.toUpperCase());
+console.log(firstname.toLowerCase());
+console.log(firstname.charAt(0).toUpperCase() + firstname.slice(5));
